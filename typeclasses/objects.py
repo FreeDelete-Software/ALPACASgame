b2e8b283787bc289aed1c8d746f3189641a4e891
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2021-12-06 10:25:40 -0600

    Re-implement move_to method for ALPACAS
    This changes the default "move_to" method for Evennia's DefaultObject
    to send "render" messages to ALPACASclient. Other than those added
    messages, it is functionally identical to the standard DefaultObject
    basetype class.

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -11,9 +11,173 @@ inheritance.
 
 """
 from evennia import DefaultObject
+from django.utils.translation import gettext as _
 
 
-class Object(DefaultObject):
+class AlpacasObject(DefaultObject):
+    """
+    Object that is configured to send custom OOB messages to ALPACASclient.
+    The AlpacasObject is meant to keep all of the same functionality and 
+    customize-ability from Evennia's DefaultObject.
+
+    This re-implements the following methods from Evennia:
+     + move_to()
+    """
+    def move_to(
+        self,
+        destination,
+        quiet=False,
+        emit_to_obj=None,
+        use_destination=True,
+        to_none=False,
+        move_hooks=True,
+        **kwargs,
+    ):
+        """
+        Moves this object to a new location.
+
+        Args:
+            destination (Object): Reference to the object to move to. This
+                can also be an exit object, in which case the
+                destination property is used as destination.
+            quiet (bool): If true, turn off the calling of the emit hooks
+                (announce_move_to/from etc)
+            emit_to_obj (Object): object to receive error messages
+            use_destination (bool): Default is for objects to use the "destination"
+                 property of destinations as the target to move to. Turning off this
+                 keyword allows objects to move "inside" exit objects.
+            to_none (bool): Allow destination to be None. Note that no hooks are run when
+                 moving to a None location. If you want to run hooks, run them manually
+                 (and make sure they can manage None locations).
+            move_hooks (bool): If False, turn off the calling of move-related hooks
+                (at_before/after_move etc) with quiet=True, this is as quiet a move
+                as can be done.
+
+        Keyword Args:
+          Passed on to announce_move_to and announce_move_from hooks.
+
+        Returns:
+            result (bool): True/False depending on if there were problems with the move.
+                    This method may also return various error messages to the
+                    `emit_to_obj`.
+
+        Notes:
+            No access checks are done in this method, these should be handled before
+            calling `move_to`.
+
+            The `DefaultObject` hooks called (if `move_hooks=True`) are, in order:
+
+             1. `self.at_before_move(destination)` (if this returns False, move is aborted)
+             2. `source_location.at_object_leave(self, destination)`
+             3. `self.announce_move_from(destination)`
+             4. (move happens here)
+             5. `self.announce_move_to(source_location)`
+             6. `destination.at_object_receive(self, source_location)`
+             7. `self.at_after_move(source_location)`
+
+        """
+        def logerr(string="", err=None):
+            """Simple log helper method"""
+            logger.log_trace()
+            self.msg("%s%s" % (string, "" if err is None else " (%s)" % err))
+            return
+
+        errtxt = _("Couldn't perform move ({err}). Contact an admin.")
+        if not emit_to_obj:
+            emit_to_obj = self
+
+        if not destination:
+            if to_none:
+                # immediately move to None. There can be no hooks called since
+                # there is no destination to call them with.
+                self.location = None
+                return True
+            emit_to_obj.msg(_("The destination doesn't exist."))
+            return False
+        if destination.destination and use_destination:
+            # traverse exits
+            destination = destination.destination
+        # Before the move, call eventual pre-commands.
+        if move_hooks:
+            try:
+                if not self.at_before_move(destination, **kwargs):
+                    return False
+            except Exception as err:
+                logerr(errtxt.format(err="at_before_move()"), err)
+                return False
+
+        # Save the old location
+        source_location = self.location
+
+        # Call hook on source location
+        if move_hooks and source_location:
+            try:
+                source_location.at_object_leave(self, destination, **kwargs)
+            except Exception as err:
+                logerr(errtxt.format(err="at_object_leave()"), err)
+                return False
+
+        if not quiet:
+            # tell the old room we are leaving
+            try:
+                self.announce_move_from(destination, **kwargs)
+            except Exception as err:
+                logerr(errtxt.format(err="at_announce_move()"), err)
+                return False
+
+        # Send message to ALPACASclient before move, regardless of "quiet" option
+        for con in self.location.contents:
+            if con.has_account:
+                con.msg(render=(("unrender",), {"obj_id": self.id, "reason": "move"}))
+
+        # Perform move
+        try:
+            self.location = destination
+        except Exception as err:
+            logerr(errtxt.format(err="location change"), err)
+            return False
+
+        # Send properties to ALPACASclient after move, regardless of "quiet" option
+        obj_properties = {
+            "display_name" : self.name,
+            "key_name" : self.key,
+            "obj_id" : self.id,
+            "obj_type" : "thing",
+            "sprite_file" : self.db.sprite_file
+        }
+        for con in self.location.contents:
+            if con.has_account:
+                con.msg(render=(("add_objects",), {"obj_list":[obj_properties]}))
+
+        if not quiet:
+            # Tell the new room we are there.
+            try:
+                self.announce_move_to(source_location, **kwargs)
+            except Exception as err:
+                logerr(errtxt.format(err="announce_move_to()"), err)
+                return False
+
+        if move_hooks:
+            # Perform eventual extra commands on the receiving location
+            # (the object has already arrived at this point)
+            try:
+                destination.at_object_receive(self, source_location, **kwargs)
+            except Exception as err:
+                logerr(errtxt.format(err="at_object_receive()"), err)
+                return False
+
+        # Execute eventual extra commands on this object after moving it
+        # (usually calling 'look')
+        if move_hooks:
+            try:
+                self.at_after_move(source_location, **kwargs)
+            except Exception as err:
+                logerr(errtxt.format(err="at_after_move"), err)
+                return False
+        return True
+
+
+class Object(AlpacasObject):
     """
     This is the root typeclass object, implementing an in-game Evennia
     game object, such as having a location, being able to be

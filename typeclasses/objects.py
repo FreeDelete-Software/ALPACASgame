a27d52caa9aa1bbd72f36be7bcfb84d8cd0af722
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2021-12-26 19:58:45 -0600

    Move at_look method to AlpacasObject

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -24,7 +24,51 @@ class AlpacasObject(DefaultObject):
     This re-implements the following methods from Evennia:
      - return_appearance()
      - move_to()
+     - at_look()
     """
+    def at_look(self, target, **kwargs):
+        """
+        Called when this object performs a look. It allows to
+        customize just what this means. It will not itself
+        send any data.
+        Args:
+            target (Object): The target being looked at. This is
+                commonly an object or the current location. It will
+                be checked for the "view" type access.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call. This will be passed into
+                return_appearance, get_display_name and at_desc but is not used
+                by default.
+        Returns:
+            lookstring (str): A ready-processed look string
+                potentially ready to return to the looker.
+        """
+        if not target.access(self, "view"):
+            try:
+                return "Could not view '%s'." % target.get_display_name(self, **kwargs)
+            except AttributeError:
+                return "Could not view '%s'." % target.key
+
+        appearance = target.return_appearance(self, **kwargs)
+
+        if isinstance(appearance, str):
+            description = appearance
+            render_list = None
+        else:
+            description = appearance.get("description")
+            render_list = appearance.get("render_list")
+
+        if target == self.location:
+            self.msg(render=(("new_room",), {"room_name":self.location.key, "room_art":self.location.db.art_file}))
+            self.msg(render=(("add_objects"), {"obj_list":render_list}))
+
+        # the target's at_desc() method.
+        # this must be the last reference to target so it may delete itself when acted on.
+        target.at_desc(looker=self, **kwargs)
+
+        return description
+
+
     def return_appearance(self, looker, **kwargs):
         """
         ALPACAS
